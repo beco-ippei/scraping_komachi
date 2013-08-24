4ab9027d4d3c6eaf4dd545c9a8a44d483c62806c
beco-ippei <beco.ippei@gmail.com>
2013-08-24 23:18:34 +0900

    add more summarizable names

--- lib/komachi_scraper.rb
+++ lib/komachi_scraper.rb
@@ -38,31 +38,123 @@ reses = doc.xpath('id("reslist")/tr/td/..').inject({}) do |h, td|
   h
 end
 
-def extract(text)
-  sori = /反町/ =~ text
-  hote = /布袋/ =~ text
-  if (sori && hote) || (!sori && !hote)
-    nil
+# keywords
+KEYWORDS = {
+  sori: %w[反町 そりまち こんな世の中じゃ],
+  hote: %w[布袋],
+  ega: %w[江頭],
+  bret: %w[Bret.Michaels ブレット.?マイケルズ] +
+    %w[ヘヴィメタのバンド ポイズン[^ガ]*バンド ビッグ・ヘア・バンド] +
+    %w[ピンクマンさんに同意],
+  rebekka: %w[レベッカ],
+  pgirl: %w[ポイズンガ[ー-]ルバンド],
+  yazawa: %w[矢沢],
+  alize: %w[Alice.Cooper アリス.?クーパー],
+  bell: %w[Bell.Biv.Devoe ベル.?ビヴ.?デヴォー],
+  jorge: %w[ボーイ.?ジョージ],
+  ayabe: %w[ピースの綾部],
+  gohayato: %w[郷隼人],
+  snow: %w[白雪姫に出てくるおばあさん],
+  jiro: %w[赤川.?次郎],
+  chasale: %w[チェーザレ・ボルジア],
+  kageinari: %w[カゲ稲荷 カゲ稲荷.*この方],
+}
+
+def extract_name(text)
+  extracted = KEYWORDS.map {|key, val|
+    /#{val.join('|')}/ =~ text ? key : nil
+  }.compact
+
+  if extracted && extracted.size == 1
+    extracted.first
   else
-    sori ? :sori : :hote
+    nil
   end
 end
+
+DECISION_WORDS = %w[です[^が] しか でした よね でしょ かな] +
+  %w[で即答 を思い浮か のほうです のポイズンで(した|す)]
+DECISION_PREFIXES = [
+  'ポイズン(の人」?)?(と|って)[い言](えば|ったら)',
+  '(ポイズン)?(の人)?.?と[い言]われれば',
+  'ポイズン(の人)?.*＝',
+]
 
 def extract_first(text)
-  if idx= text.index(/反町|布袋/)
-    text[idx..idx+1] == '反町' ? :sori : :hote
+  extracted = KEYWORDS.map {|key, val|
+    regex = [
+      /(#{val.join('|')}).{0,6}[一1１]票/,
+      /(#{val.join('|')}).{0,5}(#{DECISION_WORDS.join('|')})/,
+      /(#{DECISION_PREFIXES.join('|')})(#{val.join('|')})/,
+    ]
+    if regex.map{|r| r =~ text }.compact.first
+      key
+    end
+  }.compact
+
+  if extracted.size == 1
+    extracted.first
+  else
+    nil
   end
 end
 
-sum = reses.values.inject({}) do |h, res|
-  who = extract res[:subject]
-  who ||= extract res[:body]
-  who ||= extract_first res[:body]
+def both?(res)
+  res[:subject] + res[:body] =~ /#{[
+    '両方',
+    '二人',
+    '混ざって出てきた',
+    'ど(ちら|っち)でもな(い|かった)',
+  ].join('|')}/
+end
+def nocount?(res)
+  if res[:poster] == 'べこ妻(トピ主)'
+    true
+  elsif [res[:subject], res[:poster]] == %w[集計って！ 二度目です。]
+    true
+  else
+    false
+  end
+end
 
-  h[:who] ||= {sori: 0, hote: 0, nil => 0}
-  h[:who][who] += 1
+invalid = 0
+sum = reses.values.inject({}) do |h, res|
+  # 片方の名前しか含まれてい無い場合の取得
+  who = extract_name(res[:subject]) || extract_name(res[:body])
+
+  # 複数検出されたときは、パターンマッチで優先度
+  who ||= extract_first(res[:body])
+#    .tap {|v| puts "#{res[:body]} is #{v}" if v == :poison}
+
+  # どちらでもない？ or 両方
+  who = :both if who.nil? && both?(res)
+
+  if nocount? res
+    # 集計しない
+    invalid += 1
+#    puts "invalid ... #{res}"
+  elsif who
+    h[who] = (h[who] || 0) + 1
+  else
+    h[nil] ||= []
+    h[nil].push res
+  end
   h
 end
 
-p sum
+# output results
+names = KEYWORDS.merge(
+  both: ['両方/どちらでもない'],
+  poison: ['POISON(バンド)'],
+  pgirl: ['ポイズンガールバンド'],
+).inject({}) {|h, kv| k, v = kv; h[k] = v.first.gsub(/\.\??/, ' '); h }
+sum.select{|k,v| !k.nil? }.sort_by{|k,v| -v }.each do |k, v|
+  puts "#{names[k]} = #{v}" if k
+end
+puts "other = #{sum[nil].size}"
+
+puts "total = #{reses.size} , valid = #{reses.size - invalid}"
+
+puts "\n--- other answers ---"
+sum[nil].each {|e| p e }
 
